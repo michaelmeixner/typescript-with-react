f6e557f46124341b451a9aa1e1fc33a827b1a88a
Michael M <9michaeljm@gmail.com>
2020-12-01 14:22:31 -0500

    render prop example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TextField} from "./TextField";
+import { Counter } from './Counter';
+// import {TextField} from "./TextField";
 
 const App: React.FC = () => {
   return <div>
-    <TextField text='hell0' personObject={{lastName: 'styles', firstName: 'hairy'}} />
+    {/* <TextField text='hell0' personObject={{lastName: 'styles', firstName: 'hairy'}} /> */}
+    <Counter>
+      {(count, setCount) => (
+        <div>
+          {count}
+          <button onClick={() => setCount(count + 1)} >+</button>
+        </div>
+      )}
+    </Counter>
   </div>
 };
 
